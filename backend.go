5ff626f9078970dd05750374e4dc52d8e87993be
jan <jankretschmann1@gmail.com>
2021-02-16 00:34:29 -0600

    added functionality to return the timestamp in secs of when a word was found

--- backend.go
+++ backend.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
@@ -32,6 +33,7 @@ func getCommandAudioFromVideofile(inputFile string) *exec.Cmd{
 
 func processGET(c *gin.Context) {
 	fileLocation := c.Request.Header["Filename"][0]
+	lookingFor := c.Request.Header["Lookingfor"][0]
 	log.Print("Filelocation: " + fileLocation)
 	ctx := context.Background()
 	client, err := speech.NewClient(ctx)
@@ -44,16 +46,29 @@ func processGET(c *gin.Context) {
 
 	result, err := sendRequest(os.Stdout, client, "./files/audio.wav")
 
+	timeStamps := findWordTimestamp(lookingFor, result)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	c.JSON(200, gin.H{
-		"message": result,
+		"message": timeStamps[0],
 	})
 }
 
-func sendRequest(w io.Writer, client *speech.Client, filename string) (*string, error) {
+func findWordTimestamp(wordToFind string, audioContent *speechpb.SpeechRecognitionAlternative) []int64 {
+	results := make([]int64, 0)
+	// Think of smart way to reduce words here
+	for _, word := range audioContent.Words {
+		if word.Word == wordToFind {
+			results = append(results, word.StartTime.Seconds)
+		}
+	}
+	return results
+}
+
+func sendRequest(w io.Writer, client *speech.Client, filename string) (*speechpb.SpeechRecognitionAlternative, error) {
 	ctx := context.Background()
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -66,6 +81,7 @@ func sendRequest(w io.Writer, client *speech.Client, filename string) (*string, 
 			SampleRateHertz:	44100,
 			LanguageCode:		"en-US",
 			AudioChannelCount:	2,
+			EnableWordTimeOffsets: true,
 		},
 		Audio: &speechpb.RecognitionAudio {
 			AudioSource: &speechpb.RecognitionAudio_Content{Content: data},
@@ -76,15 +92,18 @@ func sendRequest(w io.Writer, client *speech.Client, filename string) (*string, 
 	if err != nil {
 		return nil, err
 	}
+
+	// Here is where progress bar might be good?
 	resp, err := op.Wait(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	for _, result := range resp.Results {
+		log.Print("in result loop")
 		for _, alt := range result.Alternatives {
 			fmt.Fprintf(w, "\"%v\" (confidence=%3f)\n", alt.Transcript, alt.Confidence)
 		}
 	}
-	return &resp.Results[0].Alternatives[0].Transcript, nil
+	return resp.Results[0].Alternatives[0], nil
 }
